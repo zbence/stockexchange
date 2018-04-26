404b6cbcd945c3167d3c0583140d029ad85bf1d7
zbence <zbence64@gmail.com>
2018-04-26 21:51:08 +0200

    Create stock moving stripe, add details form to selected stok

--- www/js/exchangeController.js
+++ www/js/exchangeController.js
@@ -1,7 +1,7 @@
 var exchange = angular.module("exchange", ["chart.js"]);
 
 
-
+var stockPriceCount = 10;
 
 exchange.controller('MainCtrl',["$scope","$log","$http",
  function ($scope,$log,$http) {
@@ -19,11 +19,14 @@ exchange.controller('MainCtrl',["$scope","$log","$http",
      var stock = {}; 
      var stockDetail = {};
     $scope.stockIds = [];
+    $scope.inspected = {};
+    $scope.lastPrices = {};
 
-    $http.get("api/stock/all").then(function(result){
+   $http.get("api/stock/all").then(function(result){
       
       $.each(result.data, function(value,key){
         $scope.stockIds.push(key._id);
+        $scope.stockIds = $scope.stockIds.sort();
         stock[key._id] = [];
         stockDetail[key._id] = key;
         initStockPrices();
@@ -31,13 +34,12 @@ exchange.controller('MainCtrl',["$scope","$log","$http",
       })
 
       socket.on("stockPriceUpdate",function(data){
-        console.log("New StockUpdate message: " )
-        console.log(data)
-
+        updateLastPrice(data)
         stock[data.stockId].push(data);
         console.log(stock[data.stockId].length)
         if(data.stockId == $scope.selectedStockPrice) {
           $scope.onStockChange();
+          $scope.$apply();
         }
       })
     })
@@ -46,6 +48,7 @@ exchange.controller('MainCtrl',["$scope","$log","$http",
     
 
     function initStockPrices(){
+      $scope.selectedStockPrice = $scope.stockIds[0];
       $.each(Object.keys(stock),function(value,key){
         $http.get("api/stock/price/"+key+"/quantity/100").then(
           function(result){
@@ -57,30 +60,26 @@ exchange.controller('MainCtrl',["$scope","$log","$http",
     
     
     $scope.onStockChange = function (){
-      console.log("change event")
+      
        if(!$scope.selectedStockPrice){
          console.log("selectedStockPrice is null");
          return
        }
+       $scope.selectedStockPriceCategory = stockDetail[$scope.selectedStockPrice].category;
 
        var prices = [];
        var timestamps  = [];
 
-       console.log("change event")
-       $.each(stock[$scope.selectedStockPrice],function(index,element){
+       $.each(stock[$scope.selectedStockPrice].slice(-stockPriceCount),function(index,element){
 
         prices.push(element.price);
         timestamps.push(element.timestamp);
       })
 
-      
-       console.log(prices[0])
-       $scope.data =prices;
-       console.log("change event")
-       $scope.labels =timestamps;
-       console.log("change event")
+      $scope.labels =timestamps;
      
-       $scope.$emit("$resize");
+       $scope.data =prices;
+     
        
     }
     
@@ -108,7 +107,18 @@ exchange.controller('MainCtrl',["$scope","$log","$http",
     };
 
 
-   
+    function updateLastPrice(priceObj) {
+
+      priceObj.price =  parseFloat(priceObj.price).toFixed(2);
+      if(!$scope.lastPrices[priceObj.stockId]){
+        $scope.lastPrices[priceObj.stockId] = priceObj;
+        $scope.lastPrices[priceObj.stockId].change = 0;
+      }else {
+        $scope.lastPrices[priceObj.stockId].change = parseFloat((priceObj.price - $scope.lastPrices[priceObj.stockId].price) /$scope.lastPrices[priceObj.stockId].price  *100).toFixed(2);
+        $scope.lastPrices[priceObj.stockId].price = parseFloat(priceObj.price).toFixed(2);
+      }
+
+   }
 
    
   }]);
